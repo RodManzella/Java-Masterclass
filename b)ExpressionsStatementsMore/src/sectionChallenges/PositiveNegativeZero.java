a9ddd0232779fb44137c725e293abbe7389ecde0
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-10 11:32:55 -0300

    method

--- b)ExpressionsStatementsMore/src/sectionChallenges/PositiveNegativeZero.java
+++ b)ExpressionsStatementsMore/src/sectionChallenges/PositiveNegativeZero.java
@@ -1,5 +1,18 @@
 package sectionChallenges;
 
 public class PositiveNegativeZero {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static void checkNumber(int number){
+        if(number > 0){
+            System.out.println("positive");
+        }
+        else if(number < 0){
+            System.out.println("negative");
+        }
+
+        System.out.println("zero");
+    }
 }
