0d05c3bcd27d79f70742c04fc27d31333234f638
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-09 21:14:40 -0300

    first

--- a) expressions and statements/src/1) HelloWorld/FirstClass.java
+++ a) expressions and statements/src/1) HelloWorld/FirstClass.java
@@ -1,3 +1,35 @@
 public class FirstClass{
-    
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+       /*   -the public Java keyword is what´s called an acess modifier.
+
+            - An acess modifier alows us to define which parts of our code, or even someone else´s code
+                can acess a particular element.
+
+            - The class keyword is used to define a class, and the class name will be the next following
+                the keyword, so FirstClass in this case.
+
+            - Also notice the left and right curly braces({}) they are used to define the class code block, or
+                the class body.
+
+            - So to define a class, it requires an optional acess modifier to start and in this case we´ve used the public
+                followed by the keyword class, then the class name(FirstClass) and finally followed by the left and right curly
+                braces that define the class body
+
+            - Anything inside that body is considered part of this class. Within a class, we can have data and code.
+
+            - We also see that we have a main method to this class to print out our hello world message. But first, what
+                is a method?
+
+            - A method is a collection of statements, one or more, that perform an operation.
+
+            - We´ll be using a special method called the main method, that java looks for when running a program.
+
+            - It´s the entry point for any Java code, and Java looks for the main method to start and run the program
+                when we use it.
+
+            - You can also create your own methods.
+        */
+    }
 }
