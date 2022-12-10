11c55c03c50ae4b7f9b1da988421171104357a66
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-10 11:48:34 -0300

    testing

--- b)ExpressionsStatementsMore/src/sectionChallenges/SpeedConverter.java
+++ b)ExpressionsStatementsMore/src/sectionChallenges/SpeedConverter.java
@@ -3,18 +3,19 @@ package sectionChallenges;
 public class SpeedConverter {
     
     public static void main(String[] args) {
-        long test = toMilesPerHour(1.5);
+        long test = toMilesPerHour(75.144);
         System.out.println(test);
     }
 
     public static long toMilesPerHour(double kilometersPerHour){
-        long valueReturned;
+        long valueReturned = 0;
 
         if(kilometersPerHour < 0){
             valueReturned = - 1;
         }
         else{
-            valueReturned = (long) (0.621371 * kilometersPerHour);
+            long expression = ((long) (0.621371 * kilometersPerHour));
+            valueReturned = Math.round(expression);
         }
         return valueReturned;
     }
