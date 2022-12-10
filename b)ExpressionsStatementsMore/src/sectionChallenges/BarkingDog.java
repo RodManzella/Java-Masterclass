9c2ddc0b9ec36c8c2e0bc0cfd08d70c7a6860885
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-10 12:49:57 -0300

    yes

--- b)ExpressionsStatementsMore/src/sectionChallenges/BarkingDog.java
+++ b)ExpressionsStatementsMore/src/sectionChallenges/BarkingDog.java
@@ -1,5 +1,21 @@
 package sectionChallenges;
 
 public class BarkingDog {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+        boolean isAwake;
+
+        if(barking){
+            if(hourOfDay < 0 || hourOfDay > 23){
+                isAwake = false;
+            }
+            else if(hourOfDay < 8 || hourOfDay > 22){  //Se chegou nessa condicional, não é menor que 0 e nem maior que 23
+                isAwake = true;
+            }
+        }
+        
+    }
 }
