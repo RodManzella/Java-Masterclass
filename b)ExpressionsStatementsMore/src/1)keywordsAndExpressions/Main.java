df1d867ca572351db79fcb88f3e84e051a890b16
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-10 08:12:47 -0300

    ma

--- b)ExpressionsStatementsMore/src/1)keywordsAndExpressions/Main.java
+++ b)ExpressionsStatementsMore/src/1)keywordsAndExpressions/Main.java
@@ -5,7 +5,7 @@ public class Main{
         //int int = 5;   int is a reserved keyword (can´t be a variable name)
         // int int2 = 5;   that is valid because it´s only part of the keyword.
 
-        double kilometers = (100 * 1.6093344);
+        double kilometers = (100 * 1.6093344);  //statement is the complete line(usually ends with semicolon (;))
 
         int highScore = 50;
 
@@ -18,5 +18,19 @@ public class Main{
         if ((health < 25)&& (highScore > 1000)){  // (health < 25)&& (highScore > 1000) is an expression
             highScore = highScore - 1000;  
         }
+
+
+        // To Java, whether the parts of the statement are on one line, or multiple lines, it has the same meaning, and is
+        //interpreted as a single line of code.
+
+        System.out.println("This is " +
+        " another"+
+         " still more.");                      //more readable.
+
+         //Java will ignore any whitespace.
+
+         
+
+
     }
 }
