c84073b051cbc63ba9661cc5772f2eca77736f5a
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-10 12:18:10 -0300

    converter

---
+++ b)ExpressionsStatementsMore/src/sectionChallenges/MegaBytesConverter.java
@@ -0,0 +1,20 @@
+package sectionChallenges;
+
+public class MegaBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        int megaBytes = 0, remainingKb = 0;
+
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }
+        else{
+            megaBytes = kiloBytes / 1024;
+            remainingKb = kiloBytes % 1024;
+        }
+        System.out.println(kiloBytes+" KB = "+megaBytes+" MB and "+remainingKb+ " KB" );
+    }
+}
