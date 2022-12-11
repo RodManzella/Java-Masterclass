2585da2e7357045efdb79fce901b61662f36a718
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-10 22:24:55 -0300

    area

--- b)ExpressionsStatementsMore/src/sectionChallengesTwo/AreaCalculator.java
+++ b)ExpressionsStatementsMore/src/sectionChallengesTwo/AreaCalculator.java
@@ -5,6 +5,20 @@ public class AreaCalculator {
         
     }
 
-    
-    
+    public static double area (double radius){
+        double returnValue;
+
+        if(radius < 0){
+            returnValue = -1;
+        }
+        else{
+            returnValue = Math.PI * radius * radius;
+        }
+        return returnValue;
+
+    }
+
+    public static double area (double x, double y){
+        return x * y;
+    }
 }
