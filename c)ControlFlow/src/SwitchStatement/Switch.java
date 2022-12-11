8fec309dc63569f748c57f907f16dd0b57709e71
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-11 18:43:59 -0300

    swictch

--- c)ControlFlow/src/SwitchStatement/Switch.java
+++ c)ControlFlow/src/SwitchStatement/Switch.java
@@ -3,7 +3,26 @@ package SwitchStatement;
 public class Switch {
     public static void main(String[] args) {
         
-        
+        // int value = 3;
+
+        // if(value == 1){
+        //     System.out.println("Value was 1");
+        // }else if(value == 2){
+        //     System.out.println("Value was 2");
+        // }else{
+        //     System.out.println("Was not 1 or 2");
+        // }
+
+
+        int switchValue = 1;
+
+        switch(switchValue){  //check the variable switchValue
+            case 1:                                              // In the case the variable switchValue is 1, execute the following code.
+                System.out.println("Value was 1");
+                break;  //the break tells Java that it executed the code for that case, and now wants to leave the switch block.
+            
+            
+        }
     }
     
 }
