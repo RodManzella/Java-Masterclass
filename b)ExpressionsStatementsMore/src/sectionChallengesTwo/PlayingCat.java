97a6a8e51065faff8b51bb5fe829ad63bd4f21f7
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-10 23:05:30 -0300

    code

--- b)ExpressionsStatementsMore/src/sectionChallengesTwo/PlayingCat.java
+++ b)ExpressionsStatementsMore/src/sectionChallengesTwo/PlayingCat.java
@@ -1,6 +1,28 @@
 package sectionChallengesTwo;
 
 public class PlayingCat {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        boolean isPlaying;
+
+        if(!summer){
+            if(temperature >= 25 && temperature <= 35){
+                isPlaying = true;
+            }
+            else{
+                isPlaying = false;
+            }
+            
+        }else{
+            if(temperature >= 25 && temperature <= 45){
+                isPlaying = true;
+            }
+            isPlaying = false;
+        }
+        return isPlaying;
+    }
     
 }
