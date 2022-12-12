c003b464ac6aabdbf87f823f635ca1724c2db86a
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 15:15:42 -0300

    new class

---
+++ c)ControlFlow/src/challenges/DigitSumChallenge.java
@@ -0,0 +1,13 @@
+package challenges;
+
+public class DigitSumChallenge {
+    // In this challenge, your task is to write a method, with the name sumDigits, that has a single parameter
+    // named number of type int, and it should return an int.
+
+    // The method should only take a number that is a positive number.
+    // If a negative number is passed, it should return -1, meaning an invalid value was passed.
+    //The method should parse out each digit from the number, and sum the digits up.
+    
+    // So if 125 is the value passed to the method, the code should sum each digit, in this case, 1 + 2 + 5, and return int 8.
+
+}
