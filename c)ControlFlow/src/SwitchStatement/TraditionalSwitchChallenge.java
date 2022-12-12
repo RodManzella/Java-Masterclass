70948a52315bb8da1d325e4791b7c96be7c467e7
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 09:05:48 -0300

    switch

--- c)ControlFlow/src/SwitchStatement/TraditionalSwitchChallenge.java
+++ c)ControlFlow/src/SwitchStatement/TraditionalSwitchChallenge.java
@@ -2,7 +2,35 @@ package SwitchStatement;
 
 public class TraditionalSwitchChallenge {
     public static void main(String[] args) {
-        
+        // create a new char variable.
+        // Use the traditional switch statement (with a colon in case labels) that tests the value in  the variable from step 1.
+        // - Create cases for the characters A, B, C, D and E
+        // Display a message in each case block, with the letter and the NATO word, then break.
+        // Add a default block, which displays the letter with a message saying not found.
+
+        char natoWord = 'A';
+       
+        switch(natoWord){
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+                break;
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.println("Not found");
+                break;
+                
+        }
     }
 
     
