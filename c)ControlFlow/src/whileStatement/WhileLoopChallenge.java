f5a3e9f0884a37fa12a90c70681a334aae1827bb
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 14:48:38 -0300

    class + method

---
+++ c)ControlFlow/src/whileStatement/WhileLoopChallenge.java
@@ -0,0 +1,20 @@
+package whileStatement;
+
+public class WhileLoopChallenge {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isEvenNumber(int number){
+        boolean isEven = false;;
+
+        if(number >= 0){
+            if(number % 2 == 0){
+                isEven = true;
+            }
+        }
+        return isEven;
+    }
+    
+}
