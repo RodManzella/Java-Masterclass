b74a425f0df06701888273cfa8a2743878949091
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 09:38:24 -0300

    switch

--- c)ControlFlow/src/SwitchStatement/SwitchExpressiomChallenge.java
+++ c)ControlFlow/src/SwitchStatement/SwitchExpressiomChallenge.java
@@ -1,5 +1,16 @@
 package SwitchStatement;
 
 public class SwitchExpressiomChallenge {
+    // In this challenge, we´re going to use the enhanced switch expression.
+    // We´re going to make the enhanced switch expression, by assigning it to a variable.
+
+    // Create a method called printDayOfWeek, that takes an int parameter called day, but doesn´t return any values.
+
+    // Use the enhanced switch statement, to return the name of the day, based on the parameter passed to switch statement, so that 0
+    // will return "Sunday", 1 will return "Monday", and so on. Any number not between 0 and 6, shoud return "Invalid Day".
+    // Use the enhanced switch statement as an expression, returning the result to a String named dayOfTheWeek.
+
+    
+
     
 }
