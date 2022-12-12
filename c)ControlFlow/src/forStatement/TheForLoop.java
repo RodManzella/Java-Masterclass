6b3b1bac1d7cb413a23a8b4b186d98210bd4b2c5
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 12:12:34 -0300

    new for

--- c)ControlFlow/src/forStatement/TheForLoop.java
+++ c)ControlFlow/src/forStatement/TheForLoop.java
@@ -25,6 +25,24 @@ public class TheForLoop {
             double interestAmount = calculateInterest(100.00, i);
             System.out.println("10,000 at "+ i + "% interest = $"+interestAmount);
         }
+        System.out.println("");
+
+        // Next, let´s add one more requirement, so i can introduce you to another feature of looping.
+        // Let´s say we only want to print the statement where the calculatedInterest is <= 8.5 dollars.
+        // So, in other words, we want to loop through interest rates from 7.5 to 10, but exit out of the loop if the interest amount
+        // ever gets to be more than 8.5 dollars.
+        // one option is to use the break statement: The break statement transfers control out of an encloging statement. We´ve seen the break
+        // statement in the switch statement, but it can also be used in a loop. A break statement is generally used in as in statement,
+        // testing some condition on which to break out of the loop
+
+        for(double i = 7.5; i <= 10; i += 0.25){
+            double interestAmount = calculateInterest(100.00, i);
+            if(interestAmount > 8.5){
+                break;
+            }
+            System.out.println("10,000 at "+ i + "% interest = $"+interestAmount);
+        }
+
     }
 
     public static double calculateInterest(double amount, double interestRate){
