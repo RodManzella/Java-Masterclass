63c7085e93865be7af6654e55f373dabf8097af5
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 14:45:02 -0300

    c

--- c)ControlFlow/src/whileStatement/WhileLoop.java
+++ c)ControlFlow/src/whileStatement/WhileLoop.java
@@ -22,9 +22,28 @@ public class WhileLoop {
         //     j++;
         // }
 
-       
-            
+        // int j = 1;
+        // boolean isReady = false;
 
+        // do{
+        //     if(j > 5){
+        //         break;
+        //     }
+        //     System.out.println(j);
+        //     j++;
+        //     isReady = (j > 0);
+
+        // }while(isReady);
+            
+        int number = 0;
+
+        while(number < 50){
+            number += 5;
+            if(number % 25 == 0){  //Se o número for divisível por 25, retorna para o topo da iteração.
+                continue;
+            }
+            System.out.print(number +"_");
+        }
             
     }
     
