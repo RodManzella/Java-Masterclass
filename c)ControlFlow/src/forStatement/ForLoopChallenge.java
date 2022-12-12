a44ece711d65cec5362dc7f29f1b9ee63f619896
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 12:25:19 -0300

    for loop

--- c)ControlFlow/src/forStatement/ForLoopChallenge.java
+++ c)ControlFlow/src/forStatement/ForLoopChallenge.java
@@ -1,6 +1,26 @@
 package forStatement;
 
 public class ForLoopChallenge {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isPrime(int wholeNumber){
+
+        if(wholeNumber <= 2){
+            return(wholeNumber == 2);
+        }
+
+        for(int divisor = 2; divisor < wholeNumber; divisor ++){
+            if(wholeNumber % divisor == 0){
+                return false;
+            }
+        }
+        return true;
+        // When you are using loops, it´s a good idea to see if there are any ways to cut down on iterations,
+        // especially if the code block that´s being executed is complex, or creating a lot of variables.
+
+        // One minor thing we could do, is change the code to iterate over less values.
+    }
     
 }
