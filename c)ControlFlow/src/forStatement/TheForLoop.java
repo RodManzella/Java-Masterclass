e9a4ebb376ae31d08ce1b2809cecb6d89096f77b
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 11:32:02 -0300

    for

---
+++ c)ControlFlow/src/forStatement/TheForLoop.java
@@ -0,0 +1,9 @@
+package forStatement;
+
+public class TheForLoop {
+    public static void main(String[] args) {
+        for(int counter = 1; counter <= 5; counter++){
+            System.out.println(counter);
+        }
+    }
+}
