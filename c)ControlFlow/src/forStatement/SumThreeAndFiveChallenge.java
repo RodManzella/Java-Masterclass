4109046d5940ce80b35996e7ba36935fcd4927f9
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 13:08:08 -0300

    aaa

--- c)ControlFlow/src/forStatement/SumThreeAndFiveChallenge.java
+++ c)ControlFlow/src/forStatement/SumThreeAndFiveChallenge.java
@@ -7,7 +7,7 @@ public class SumThreeAndFiveChallenge {
         // Print out the numbers that have met the above conditions.
         // Break out of the loop once you have found 5 numbers that met the conditions above
         // after breaking out of the loop, print the sum of the numbers that met the conditions above.
-        int acumulator = 0, counter = 1;
+        int acumulator = 0, counter = 0;
 
         for(int loopNumber = 1; loopNumber <= 1000; loopNumber++){
             
