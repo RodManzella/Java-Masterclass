705cb23f8d18a3e738458251a5f4c26cf87d25eb
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 14:02:49 -0300

    new functiom

--- c)ControlFlow/src/forStatement/challenges/SumOdd.java
+++ c)ControlFlow/src/forStatement/challenges/SumOdd.java
@@ -6,6 +6,25 @@ public class SumOdd {
         
     }
     
+    public static int sumOdd(int start, int end){
+        int acumulator = 0;
+
+        if(end < start){
+            return -1;
+        }else if(start < 0){
+            return -1;
+        }else if(end < 0){
+            return -1;
+        }
+
+        for(int i = start; i <= end; i++){
+            if(isOdd(i)){
+                acumulator += i;
+            }
+        }
+        return acumulator;
+    }
+
     public static boolean isOdd(int number){
         boolean isOdd = false;
 
