14512db9f5a26942ad01054a7ca4e0a8cc96bcd2
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 10:58:41 -0300

    challenge

--- c)ControlFlow/src/SwitchStatement/NumberOfDaysInMonth.java
+++ c)ControlFlow/src/SwitchStatement/NumberOfDaysInMonth.java
@@ -2,7 +2,7 @@ package SwitchStatement;
 
 public class NumberOfDaysInMonth {
     public static void main(String[] args) {
-        
+        System.out.println(getDaysInMonth(2, 2000));
         
     }
 
@@ -23,9 +23,32 @@ public class NumberOfDaysInMonth {
         if(month < 1 || month > 12){
             daysInMonth = -1;
         }
-        else if(daysInMonth){
-            
+        else if(month < 1 || month > 9999){
+            daysInMonth = -1;
         }
+        else{
+            daysInMonth = switch(month){
+                case 1 -> 31;
+                case 2 -> {
+                    if(leapYear){
+                        yield 29;
+                    }
+                    yield 28;
+                }
+                case 3 -> 31;
+                case 4 -> 30;
+                case 5 -> 31;
+                case 6 -> 30;
+                case 7 -> 31;
+                case 8 -> 31;
+                case 9 -> 30;
+                case 10 -> 31;
+                case 11 -> 30;
+                case 12 -> 31;
+                default -> throw new IllegalArgumentException("Unexpected value: " + month);
+            };
+        }
+        return daysInMonth;
     }
 
 
