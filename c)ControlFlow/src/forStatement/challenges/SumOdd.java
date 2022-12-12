a10de93e72c8088af807e91239e25a2c6df0ff83
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 13:49:29 -0300

    class

--- c)ControlFlow/src/forStatement/challenges/SumOdd.java
+++ c)ControlFlow/src/forStatement/challenges/SumOdd.java
@@ -1,5 +1,19 @@
 package forStatement.challenges;
 
 public class SumOdd {
+
     
+    public static boolean isOdd(int number){
+        boolean isOdd = false;
+
+        if(number < 0 ){
+            return isOdd;
+        }
+        else{
+            if(number % 2 != 0){
+                isOdd = true;
+            }
+        }
+        return isOdd;
+    }
 }
