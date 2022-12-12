ae74bcef3015177b83b9a2ed37c8aab9826a7fb2
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 11:35:25 -0300

    testing

--- c)ControlFlow/src/forStatement/TheForLoop.java
+++ c)ControlFlow/src/forStatement/TheForLoop.java
@@ -5,5 +5,12 @@ public class TheForLoop {
         for(int counter = 1; counter <= 5; counter++){
             System.out.println(counter);
         }
+
+        System.out.println("10,000 at 2% interest = "+ calculateInterest(10000.0, 2.0));
+    }
+
+    public static double calculateInterest(double amount, double interestRate){
+
+        return (amount * (interestRate / 100));
     }
 }
