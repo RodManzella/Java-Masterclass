529aa884a6e983c43c5e8fc3b840a21ef6af451b
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 10:42:36 -0300

    new challenge

--- c)ControlFlow/src/SwitchStatement/NumberOfDaysInMonth.java
+++ c)ControlFlow/src/SwitchStatement/NumberOfDaysInMonth.java
@@ -3,10 +3,10 @@ package SwitchStatement;
 public class NumberOfDaysInMonth {
     public static void main(String[] args) {
         
+        
     }
 
     public static boolean isLeapYear(int year){
-
         boolean isLeap = false;
         if(year >= 1 && year <= 9999){
             if(year % 400 == 0){
@@ -14,6 +14,18 @@ public class NumberOfDaysInMonth {
             }
         }
         return isLeap;
+    }
+
+    public static int getDaysInMonth(int month, int year){
+        int daysInMonth;
+        boolean leapYear = isLeapYear(year);
+
+        if(month < 1 || month > 12){
+            daysInMonth = -1;
+        }
+        else if(daysInMonth){
+            
+        }
     }
 
 
