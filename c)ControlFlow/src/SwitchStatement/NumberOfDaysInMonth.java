9fd7c8effb281db5a3b5f5482821cf5ab0c8a351
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-12 10:27:03 -0300

    testing

--- c)ControlFlow/src/SwitchStatement/NumberOfDaysInMonth.java
+++ c)ControlFlow/src/SwitchStatement/NumberOfDaysInMonth.java
@@ -1,5 +1,20 @@
 package SwitchStatement;
 
 public class NumberOfDaysInMonth {
-    
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isLeapYear(int year){
+
+        boolean isLeap = false;
+        if(year >= 1 && year <= 9999){
+            if(year % 400 == 0){
+                isLeap = true;
+            }
+        }
+        return isLeap;
+    }
+
+
 }
