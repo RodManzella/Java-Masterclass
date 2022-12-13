da81222f9d1dc14db55b0e30545d8826b53140a8
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-13 08:50:22 -0300

    new

--- c)ControlFlow/src/challenges/NumberPalindrome.java
+++ c)ControlFlow/src/challenges/NumberPalindrome.java
@@ -1,5 +1,25 @@
 package challenges;
 
 public class NumberPalindrome{
-    
+    public static void main(String[] args) {
+        boolean palindrome = isPalindrome(101);
+        System.out.println(palindrome);
+        
+    }
+
+    public static boolean isPalindrome(int number){
+        int reverse = 0;
+        boolean isNumberPalindrome = false;
+
+        while(number != 0){
+            reverse = (reverse * 10) +  (number % 10);
+            number = number / 10;
+        }
+
+        if(reverse == number){
+            isNumberPalindrome = true;
+        }
+        return isNumberPalindrome;
+        
+    }
 }
