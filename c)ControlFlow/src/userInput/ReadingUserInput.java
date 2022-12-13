d7c2984301c1e12eaf5b3388ac2e66dfa29c6f2d
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-13 10:12:12 -0300

    class

--- c)ControlFlow/src/userInput/ReadingUserInput.java
+++ c)ControlFlow/src/userInput/ReadingUserInput.java
@@ -3,6 +3,16 @@ package userInput;
 public class ReadingUserInput {
     public static void main(String[] args) {
         
+        int currentYear = 2022;
+        String usersDateOfBirth = "1999";
+
+        int dateOfBirth = Integer.parseInt(usersDateOfBirth);  //Parsing String userDateOfBirth to integer
+        // We know that Integer is a class, it´s not an object. On this class, there is a static method called parseInt, that
+        // takes a String and returns an Integer. And we just saw that, to acess static methods on a class, we have to use the Class
+        // name and the dot notation with the name of the method, so, in other words, what we´re really doing is we´re calloing a method
+        // on the class named Integer.
+
+        System.out.println("Age = "+(currentYear - dateOfBirth));
     }
     
 }
