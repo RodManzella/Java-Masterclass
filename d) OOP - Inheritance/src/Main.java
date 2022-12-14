d342cc2f9d1757baed347393386420f24ede7c81
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-14 13:33:02 -0300

    text and main class

--- d) OOP - Inheritance/src/Main.java
+++ d) OOP - Inheritance/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+
         car.setMake("Porsche");  //Setting the atribute make(String) to "Porsche of the instance car".
         car.setModel("Carrera");
         car.setDoors(2);
