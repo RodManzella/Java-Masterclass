6cd59f788be88846b2343cd4cb183e2d4fcc7b3b
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-14 15:32:34 -0300

    comments

--- d) OOP - Inheritance/src/firstObjectOrientedChallenge/BankAccount.java
+++ d) OOP - Inheritance/src/firstObjectOrientedChallenge/BankAccount.java
@@ -2,4 +2,22 @@ package firstObjectOrientedChallenge;
 
 public class BankAccount {
     
+    
 }
+
+// Create a new class for a bank account.
+
+// Create fields for account characteristics like:
+    //account number
+    //account balance
+    //customer name
+    //email
+    //phone number
+
+// Create getters and settes for each field.
+// create two additional methods:
+    //one for depositing funds into the account
+    //one for withdrawing funds from the account
+
+// A customer should not be allowed to withdraw funds, if that withdrawal takes their balance negative.
+// You´ll create an instance of account class, and then test your withdrawal and deposit methods.
