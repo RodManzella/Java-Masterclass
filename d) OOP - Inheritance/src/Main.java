feeb05f99f44aaa5858e03e5ba24552f2a5332a9
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-14 12:41:57 -0300

    comments

--- d) OOP - Inheritance/src/Main.java
+++ d) OOP - Inheritance/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Car car = new Car();
+        // car.make = "Porsche";
+        // car.model =  "Carrera";
+        // car.color = "Red";
+        // System.out.println("make = "+ car.make);
+        // System.out.println("model = "+car.model);  //How do we get acess to those private fields?
+
+        //What we´re going to do is allow acess to this data, either to set it, or get it through methods on this class.
+        // These are a special set of methods, called getters, and setters. So, what are getters and setters?(see txt)
+
         car.describeCar();
     }
 }
