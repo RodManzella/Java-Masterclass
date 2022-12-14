3eaf5d3db6462b6aaf0fe7763c21f3dade4fa3d3
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-14 11:22:54 -0300

    comment

--- d) OOP - Inheritance/src/Car.java
+++ d) OOP - Inheritance/src/Car.java
@@ -1,3 +1,21 @@
 public class Car{
     
+
+
+
+
+
+
+    // what we need to do is create some variables, that are part of this class.
+    // Now to date, you´ve seen variables used inside a method or code block.
+    // And these were called local variables, because they were local to, or actually belonged to, that method or code block.But
+    // we couldn´t acess those local variables outside of the method, or block we declared them. Classes allow us to create variables
+    // that can be seen or acessible, by any code block within that class. But we can also allow acess from outside the class itself
+    // When we´re designing our class, there are some things we want the public to know, and some things that aren´t necessary for the
+    // public to know. We can have a public interface. This is only the information the oustide world needs to know, to use our class.
+    // But we´ll also have a non-public interface. This information we may want share, but not always, and not with everyone. We may need
+    // to share some information with our own company and other departments, while other data might need to be shared with our manufactureers
+    // and dealders, but not with the public. Java gives us the ability to have this kind of control, by specifyng different acess modifiers
+    // for each member in a class. (more information about acess modifiers for class member in the txt file)
+    
 }
