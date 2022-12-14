b26b6c6f71e58bc22e1d472b95abc6d5ff23ff08
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-14 15:38:55 -0300

    deposit

--- d) OOP - Inheritance/src/firstObjectOrientedChallenge/BankAccount.java
+++ d) OOP - Inheritance/src/firstObjectOrientedChallenge/BankAccount.java
@@ -1,6 +1,49 @@
 package firstObjectOrientedChallenge;
 
 public class BankAccount {
+    private String accountNumber;
+    private double accountBalance;
+    private String nameOfAccountHolder;
+    private String emailOfAccountHolder;
+    private String phoneOfAccountHolder;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+    public String getNameOfAccountHolder() {
+        return nameOfAccountHolder;
+    }
+    public void setNameOfAccountHolder(String nameOfAccountHolder) {
+        this.nameOfAccountHolder = nameOfAccountHolder;
+    }
+    public String getEmailOfAccountHolder() {
+        return emailOfAccountHolder;
+    }
+    public void setEmailOfAccountHolder(String emailOfAccountHolder) {
+        this.emailOfAccountHolder = emailOfAccountHolder;
+    }
+    public String getPhoneOfAccountHolder() {
+        return phoneOfAccountHolder;
+    }
+    public void setPhoneOfAccountHolder(String phoneOfAccountHolder) {
+        this.phoneOfAccountHolder = phoneOfAccountHolder;
+    }
+
+    public void deposit(double depositAmount){
+        accountBalance += depositAmount;
+        System.out.println("Deposit of $"+depositAmount+" made. New balance is $"+ accountBalance);
+    }
+
+    
     
     
 }
@@ -21,3 +64,4 @@ public class BankAccount {
 
 // A customer should not be allowed to withdraw funds, if that withdrawal takes their balance negative.
 // You´ll create an instance of account class, and then test your withdrawal and deposit methods.
+// You´ll print information to the console, that confirms what the balance is after the methods are called.
