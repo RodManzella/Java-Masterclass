dc47d02e78136a587051505937637f5013f6bb93
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-14 11:46:02 -0300

    more comments

--- d) OOP - Inheritance/src/Car.java
+++ d) OOP - Inheritance/src/Car.java
@@ -1,12 +1,33 @@
 public class Car{
+    private String make;
+    private String model;
+    private String color;
+    private int doors;
+    private boolean convertible;
+
+
+    // Here we created five fields.
+    // These are fields, because they are defined in the class´s code block, or the body of the class, and not in a method.
+    // When we create an object from this class, then the values we assign to these fields represent the state of the object.
+    // Unlike local variables, class variables should have some type of acess modifier declared for it.
+    // If you don´t declare one, Jaca declares the default one(package private), implicitly.
+
+    // So, here we´ve set the acess modifier to be private in all cases, which we´ve said will help us encapsulate the class.
+    // We´ll want to control acess to these fields, and this starts by making them private. Later we´ll add the methods to acess them.
+    // The other thing you might have noticed, is that we´re not actually assigning any values yet.
+    // This is because, at this point, we don´t know what these values might be, and they´ll likely be different for each instance anyway.
+    // If we were creating creating a real application, we´d likely have a lot more fields, but we´ll keep this simple to start with.
+    // Let´s add a method now that will print out this information about the car object.
+    // We´ll call this method describeCar, and make the method public.
+    // This method is not static, because we´re acessing instance fields on the class.
+    // Methods, unlike fields, will often be public, because we want to give users a way to interact with the object.
+
     
+ 
+}
 
 
-
-
-
-
-    // what we need to do is create some variables, that are part of this class.
+// what we need to do is create some variables, that are part of this class.
     // Now to date, you´ve seen variables used inside a method or code block.
     // And these were called local variables, because they were local to, or actually belonged to, that method or code block.But
     // we couldn´t acess those local variables outside of the method, or block we declared them. Classes allow us to create variables
@@ -17,5 +38,3 @@ public class Car{
     // to share some information with our own company and other departments, while other data might need to be shared with our manufactureers
     // and dealders, but not with the public. Java gives us the ability to have this kind of control, by specifyng different acess modifiers
     // for each member in a class. (more information about acess modifiers for class member in the txt file)
-    
-}
