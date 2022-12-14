e97467baaf2d844a6d7025c3b9aa68133cdaf954
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-14 15:50:57 -0300

    example

--- d) OOP - Inheritance/src/firstObjectOrientedChallenge/Main.java
+++ d) OOP - Inheritance/src/firstObjectOrientedChallenge/Main.java
@@ -1,5 +1,18 @@
 package firstObjectOrientedChallenge;
 
 public class Main {
+    public static void main(String[] args) {
+        
+        BankAccount bobsAccount = new BankAccount();
+        bobsAccount.setAccountNumber("12345");
+        bobsAccount.setAccountBalance(1000.00);
+        bobsAccount.setNameOfAccountHolder("Bob Brown");
+        bobsAccount.setEmailOfAccountHolder("myemail@bob.com");
+        bobsAccount.setPhoneOfAccountHolder("(087) 123-4567");
+
+        bobsAccount.withdrawal(100);
+        bobsAccount.deposit(250);
+        bobsAccount.withdrawal(50);
+    }
     
 }
