cfd4bc243755485ac2c59510e27a9b3c975b0fb3
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-20 12:44:28 -0300

    removing warning

--- d) OOP - Inheritance/src/firstObjectOrientedChallenge/Main.java
+++ d) OOP - Inheritance/src/firstObjectOrientedChallenge/Main.java
@@ -22,8 +22,8 @@ public class Main {
         bobsAccount.deposit(250);
         bobsAccount.withdrawal(50);
 
-        BankAccount timsAccount = new BankAccount("Tim", "tim@email.com",
-         "12345");
+        // BankAccount timsAccount = new BankAccount("Tim", "tim@email.com",
+        //  "12345");
 
          
     }
