e9ee453c294fa9199c9fcee7cba41b84fb2e0839
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-22 17:34:10 -0300

    a

--- d) OOP - Inheritance/src/Inheritance/Dog.java
+++ d) OOP - Inheritance/src/Inheritance/Dog.java
@@ -1,10 +1,31 @@
 package Inheritance;
 
 public class Dog extends Animal {
+
+    private String earShape;
+    private String tailShape;
+
     
     public Dog(){
         super("Mutt", "Big", 50);
     }
+
+    public Dog(String type,  double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small": (weight < 35 ? "medium": "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog [earShape=" + earShape + ", tailShape=" + tailShape + "]" + super.toString();
+    }
+
+    
+
+    
+
+    
 }
 
 // Using extends specifies the superclass (or the parent class) of the class we´re declaring.
