0ad6edd1f4c32beb8458cf17f99a44ef6ac311cb
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-23 12:06:08 -0300

    aaa

--- d) OOP - Inheritance/src/inheritanceChallenge/Employee.java
+++ d) OOP - Inheritance/src/inheritanceChallenge/Employee.java
@@ -5,13 +5,18 @@ public class Employee extends Worker {
     // This was because we created the default constructor for Worker, so Java can create a default constructor for Employee with its own
     // implied call to the empty constructor.
 
+    // Let´s not pass employeeId on the constructor. Let´s generate it. We can do this by setting a static field called employee number
+    // on Employee.
+
     private long employeeId;
     private String hireDate;
+    private static int employeeNo = 1;
     
 
-    public Employee(String name, String birthDate, long employeeId, String hireDate) {
+    public Employee(String name, String birthDate,  String hireDate) {
         super(name, birthDate);
-        this.employeeId = employeeId;
+        this.employeeId = Employee.employeeNo ++;
+        // Using the class name when using a static field helps people reading this code understand what´s ocurring.
         this.hireDate = hireDate;
     }
 
