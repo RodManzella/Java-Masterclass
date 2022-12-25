13f8c624f9a891f45438576691a0249c6d8a398f
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-25 14:10:25 -0300

    yes

--- d) OOP - Inheritance/src/stringFormatting/Main.java
+++ d) OOP - Inheritance/src/stringFormatting/Main.java
@@ -4,6 +4,24 @@ public class Main{
 
     public static void main(String[] args) {
         
+        String bulletIt = "Print a Bulleted List:\n" +
+        "\t\u2022 First Point \n" +
+        "\t\t\u2022 Sub Point";
+
+        System.out.println(bulletIt);
+
+
+
+        String textBlock = """
+                Print a Bulleted List:
+                            \u2022 First point
+                                \u2022 Sub Point """;
+        System.out.println(textBlock);
 
     }
+
+    //         \t        insert a tab character.
+    //         \n        insert a new line character.
+    //         \"        insert a double quote character.
+    //         \\        insert a backlash character.
 }
