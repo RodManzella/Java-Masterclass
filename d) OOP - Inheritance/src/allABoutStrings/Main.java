f744cd64ae499aa65e2ea8ef4b002eb2f72a3f90
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-25 15:13:01 -0300

    main

---
+++ d) OOP - Inheritance/src/allABoutStrings/Main.java
@@ -0,0 +1,55 @@
+package allABoutStrings;
+
+public class Main {
+    public static void main(String[] args) {
+        printInformation("Hello world");
+        printInformation("");
+        printInformation("\t   \n");
+
+        String helloWorld =  "Hello World";
+        System.out.printf("index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("index of World = %d %n", helloWorld.indexOf("World"));
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l'));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l'));
+        //The first method, indexOf, gave us the position of the first L in Hello World.
+        // The second method, lastIndexOf, gave us the position of the last L, in Hello World.
+
+        // We found the first L at position 2. If we wanted to start looking for the next L, we´d start looking after that, at index 3.
+        // We can pass 3 as the second argument to the indexOf method.
+        // And we can do something similar with the lastIndexOf method. We said the last L was at index 9, and since this method searches
+        // forward for the character, and not backwars, we´ll use 8 as the second argument.
+
+        System.out.printf("index of l = %d %n", helloWorld.indexOf('l', 3));
+        System.out.printf("index of l = %d %n", helloWorld.lastIndexOf('l', 8));
+
+        // both of these calls find the 2nd L in the String, using different methods.
+
+        // The indexOf method found it looking forward, starting at index 3, and searching towards the end of the String.
+        // The lastIndexOfMethod found it looking backwards, starting from infex 8, and searching towards thje beginning of the String.
+
+
+        //---------------------------------String comparison Methods----------------------------------------------------------------------
+
+        
+
+
+    }
+
+    public static void printInformation(String string){
+        int length = string.length();
+        System.out.printf("Length = %d %n", length);
+        if(string.isEmpty()){
+            System.out.println("String is Empty");
+            return;
+        }
+
+        if(string.isBlank()){
+            System.out.println("String is Blank");
+        }
+
+        System.out.printf("First char = %c %n", string.charAt(0));
+
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
+    }
+}
