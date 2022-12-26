66a9c7dca50bebc085258dd8a9be2e3e2532b11c
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-26 11:36:35 -0300

    methods

--- d) OOP - Inheritance/src/allABoutStrings/StringMethods.java
+++ d) OOP - Inheritance/src/allABoutStrings/StringMethods.java
@@ -1,8 +1,17 @@
 package allABoutStrings;
 
 public class StringMethods {
-    
+
     public static void main(String[] args) {
         
+        String birthdate = "25/11/1982";
+        int startingIndex = birthdate.indexOf("1982");
+        System.out.println("startingIndex = "+ startingIndex);
+        System.out.println("Birth year =  "+birthdate.substring(startingIndex));
+
+        System.out.println("Month = "+ birthdate.substring(3, 5));
+
+        String newDate = String.join("/", "25" , "11", "1982");
+        System.out.println("newDate = "+newDate);
     }
 }
