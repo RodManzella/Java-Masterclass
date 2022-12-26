ea9947f70d70b7e70b437e6dafa8e29db04b529e
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-26 20:11:21 -0300

    main

--- d) OOP - Inheritance/src/stringOptions/Main.java
+++ d) OOP - Inheritance/src/stringOptions/Main.java
@@ -42,13 +42,33 @@ public class Main {
         // Now, we´ll create two more StringBuilder instances. This time, we´ll make them both start with empty character sequences.
 
         StringBuilder emptyStart =  new StringBuilder();
+        emptyStart.append("a".repeat(17));
+        // for the first StringBuilder object, which has a capacity of 16, this is going to require  a request for a larger memory area,
+        // to store the extra data. Let´s now run this.
+
+        // First, look at the value in the StringBuilder objects, both contain a String of a´s, and both have a length of 17, as expected.
+        // But in the first case, the capacity is now 34(when we started out with 16). The new allocation size is determined by the JVM.
+        // In the second case, the capacity is still 32, because we didn´t require reallocation, since the appended string still fit in the
+        // original capacity. What happens if we change our code in the first instance to something bigger than 34 characters?
+        // Let´s change our first call to repeat, to create a String of 57 characters. Now, the output show that the capacity of the first
+        // empty StringBuilder, is actually the same size, as the String we passed in, 57.
+        // At this stage, it´s not important that you understand how the next capacity is determined. But is important to understand that the
+        //capacity does need to adjust, as the text in your StringBuilder grows.
+
+        // If you know you plan to house a large text value in your StringBuilder object, start out with a larger capacity.
+
         StringBuilder emptyStart32 = new StringBuilder(32);
+        emptyStart32.append("a".repeat(17));
 
         printInformation(emptyStart);
         printInformation(emptyStart32);
 
         // A stringBuilder is mutable, meaning it can shrinkn or grow, in size. By default, an empty StringBuilder starts with a capacity
-        // of 16, meaning it can contain up to 16 characters, before it needs to request more memory.
+        // of 16, meaning it can contain up to 16 characters, before it needs to request more memory. In the second example, we created a
+        //  a StringBuilder with a starting capacity of 32, which means our sequence can grow up to 32 characters, without needing to request 
+        // additional allocation. Every time a StringBuilder needs to increase capacity, the data stored in the original storage, needs to get
+        // copied over to the larger storage area. Let´s see this in action.
+        // Let´s append some text to our two empty StringBuilder objects.
     }
 
     public static void printInformation(String string){
