83f4e1fe94e795e99da7ae71499f7b9b87bb7782
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-26 21:35:49 -0300

    classes

--- e) OOP - Polymorphism/src/inheritanceVsComposition/Main.java
+++ e) OOP - Polymorphism/src/inheritanceVsComposition/Main.java
@@ -10,11 +10,17 @@ public class Main {
 
         PersonalComputer thePC = new PersonalComputer("2208", "Dell", theCase, theMonitor, theMotherboard);
 
-        thePC.getMonitor().drawPixelAt(10, 10, "red");
-        thePC.getMotherboard().loadProgram("Windows OS");
-        thePC.getComputerCase().pressPowerButton();
+        // thePC.getMonitor().drawPixelAt(10, 10, "red");
+        // thePC.getMotherboard().loadProgram("Windows OS");
+        // thePC.getComputerCase().pressPowerButton();
+
         // Here, in all three cases, we´re ultimately accessing functions in other classes, theCase, theMotherboard and the monitor, but
         // we´re  acessing them through thePC object.
+
+        // We really don´t want the Main class, or any class except thePersonalComputer, to make calls on its parts.
+        // Okay, so next, let´s create methods on PersonalComputer, which will expose to the calling code.
+
+        thePC.powerUp();
         
     }
 
