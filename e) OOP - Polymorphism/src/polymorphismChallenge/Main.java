34c72544f78a016d298279881fa97d3c9b7d6b9b
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-12-29 13:52:31 -0300

    aaa

--- e) OOP - Polymorphism/src/polymorphismChallenge/Main.java
+++ e) OOP - Polymorphism/src/polymorphismChallenge/Main.java
@@ -10,6 +10,15 @@ public class Main {
         Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 15.4, 6);
         runRace(ferrari);
 
+        Car tesla = new ElectricCar("2022 Red Tesla Model 3", 568, 75);
+        runRace(tesla);
+
+        Car ferrariHybrid = new HybridCar("2022 bLACK fERRARI sf90 Stradale",16, 8, 8);
+        runRace(ferrariHybrid);
+
+        // In this example, we created a polymorphic method on the Main class. This method only knew about the Car class, and not about any of its
+        // subclasses. And it could be executed, for many different types as we showed.
+
         
     }
 
