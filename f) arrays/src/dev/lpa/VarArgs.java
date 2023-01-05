fefcec04080e91be57789c6286ae4accc3957c53
Rodrigo Manzella <manzellateixeira@gmail.com>
2023-01-04 21:19:57 -0300

    new class

---
+++ f) arrays/src/dev/lpa/VarArgs.java
@@ -0,0 +1,27 @@
+package dev.lpa;
+
+public class VarArgs {
+    
+    public static void main(String[] args) {
+        
+        
+
+
+
+
+
+
+
+        /*
+         *   public static void main(String... args){
+         * 
+         *      // This is a special designation for Java, that means, Java will take zero, one, or many Strings, as arguments to this
+         *      // method, and create an array with with which to process them, in the method.
+         *   }
+         * 
+         * // The array will be called args, and be of type String.
+         * // So what´s the difference then?
+         * // The difference is minor, within the method body, but significant to the code that calls the method.
+         */
+    }
+}
